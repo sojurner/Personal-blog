0c84884970746b27cbd2a6c7cb533558079d2cc7
Paul Kim <paul.kim@catalystrms.com>
2020-12-20 18:19:53 -0700

    Update firebase env vars

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,14 +3,14 @@ import "firebase/database" // importing the auth module as an example
 
 // Firebase web config
 const config = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
-  measurementId: process.env.MEASUREMENT_ID,
+  apiKey: process.env.GATSBY_API_KEY,
+  authDomain: process.env.GATSBY_AUTH_DOMAIN,
+  databaseURL: process.env.GATSBY_DATABASE_URL,
+  projectId: process.env.GATSBY_PROJECT_ID,
+  storageBucket: process.env.GATSBY_STORAGE_BUCKET,
+  messagingSenderId: process.env.GATSBY_MESSAGING_SENDER_ID,
+  appId: process.env.GATSBY_APP_ID,
+  measurementId: process.env.GATSBY_MEASUREMENT_ID,
 }
 
 let instance = null
