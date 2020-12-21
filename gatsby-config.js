61fd63f2ebd901efc9c58229b3eae09d81bd27de
Paul Kim <paul.kim@catalystrms.com>
2020-12-20 17:26:58 -0700

    Add Firebase config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,9 @@
 const path = require("path")
 
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
     title: "PK",
@@ -95,5 +99,19 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-dark-mode",
     "gatsby-plugin-transition-link",
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: process.env.API_KEY,
+          authDomain: process.env.AUTH_DOMAIN,
+          databaseURL: process.env.DATABASE_URL,
+          projectId: process.env.PROJECT_ID,
+          storageBucket: process.env.STORAGE_BUCKET,
+          messagingSenderId: process.env.MESSAGING_SENDER_ID,
+          appId: process.env.APP_ID,
+        },
+      },
+    },
   ],
 }
