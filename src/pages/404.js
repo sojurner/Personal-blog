1475517f561a48e22615b5185f4c93bf0900b8bf
Paul Kim <paul.kim@catalystrms.com>
2021-03-23 07:46:20 -0600

    Add 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,24 +1,24 @@
 import React from "react"
 
-import { OldManAndTheSea } from "@components/Svg"
 import Flex from "@components/Flex"
 import Typography from "@components/Typography"
 import MainLayout from "@components/Layouts"
 
 import "@styles/pages/_404Page.scss"
+import f04 from "../images/404Patrol.jpeg"
 
 const Four0Four = () => {
   return (
     <MainLayout className="page-404">
-      <Typography className="page-404__title" tag="h1">
+      <Typography className="page-404__title" variant="defaultDark" tag="h1">
         Page Not Found!
       </Typography>
       <Flex classes={["flexRow", "justifyContentCenter", "alignItemsCenter"]}>
-        <Typography className="page-404__4" tag="h1">
+        <Typography className="page-404__4" variant="negativeDefault" tag="h1">
           4
         </Typography>
-        <OldManAndTheSea className="page-404__feature-img" />
-        <Typography className="page-404__4" tag="h1">
+        <img alt="404 patrol" src={f04} className="page-404__feature-img" />
+        <Typography className="page-404__4" variant="negativeDefault" tag="h1">
           4
         </Typography>
       </Flex>
